b6149ef2d8d8d0fb398b268a709566847c0e2553
Oleg <roxvemm2014@gmail.com>
2021-11-15 22:05:59 -0800

    commit tree 4a5f7a15cb20b470a94b6fba5dc65fb178964b8f

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,8 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # added rescue_from
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
 
   # GET /birds
   def index
@@ -8,11 +11,29 @@ class BirdsController < ApplicationController
   end
 
   # POST /birds
+  # def create
+  #   bird = Bird.create(bird_params)
+  #   render json: bird, status: :created
+  # end
+
+  # POST /birds
+  # def create
+  #   bird = Bird.create(bird_params)
+  #   if bird.valid?
+  #     render json: bird, status: :created
+  #   else
+  #     render json: { errors: bird.errors }, status: :unprocessable_entity
+  #   end
+  # end 
+
+  # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    # create! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
   end
-
+  
+  
   # GET /birds/:id
   def show
     bird = find_bird
@@ -20,11 +41,30 @@ class BirdsController < ApplicationController
   end
 
   # PATCH /birds/:id
+  # def update
+  #   bird = find_bird
+  #   bird.update(bird_params)
+  #   render json: bird
+  # end
+
+    # PATCH /birds/:id
+  # def update
+  #   bird = find_bird
+  #   bird.update!(bird_params)
+  #   render json: bird
+  # rescue ActiveRecord::RecordInvalid => invalid
+  #   render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  # end 
+
+      # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
+    # update! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    bird.update!(bird_params)
     render json: bird
-  end
+  end 
+  
+  
 
   # DELETE /birds/:id
   def destroy
@@ -46,5 +86,10 @@ class BirdsController < ApplicationController
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
   end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+
 
 end
